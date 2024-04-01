63d35112c149cd426f568114c5086c89fccbcfd2
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2024-04-01 12:29:55 +0400

    add sample scripts for plots

---
+++ scripts/lineShapesComparison.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+wavenumber, lineShape1 = np.loadtxt(fname='./lineShape2Data.dat', unpack=True, delimiter=',')
+wavenumber, lineShape2 = np.loadtxt(fname='./lineShape1Data.dat', unpack=True, delimiter=' ')
+
+# wavenumber, lineShape1, lineShape2 = np.loadtxt(fname='./lineShapesData.dat', unpack=True, delimiter=',')
+
+ax, fig = plt.subplots()
+
+small_value = 1e-45
+lineShape2[lineShape2 < small_value] = small_value
+
+ax.plot(wavenumber, np.log(lineShape1), color='r', label='LOR')
+ax.plot(wavenumber, np.log(lineShape2), color='b', label='DOP')
+
+ax.set_xlabel(r'wavenumber, $\mathregular{cm^{-1}}$')
+ax.set_ylabel(r'$k,\,\mathregular{cm}$')
+ax.grid(which='major', axis='both', color='gray', alpha=0.5)
+
+# Uncomment for setting a title
+# plt.title()
+
+plt.tight_layout()
+plt.show()
---
+++ scripts/spectraComparison.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+wavenumber, absorptionCoeff = np.loadtxt(fname='./ObtainedData.dat', unpack=True, delimiter=',')
+wavenumber, refAbsorptionCoeff = np.loadtxt(fname='./ReferenceData.dat', unpack=True, delimiter=' ')
+
+fig, ax = plt.subplots()
+
+ax.plot(wavenumber, absorptionCoeff, color='b', label='Obtained data')
+ax.plot(wavenumber, refAbsorptionCoeff, color='r', label='Reference data')
+
+ax.set_xlabel(r'wavenumber, $\mathregular{cm^{-1}}$')
+ax.set_ylabel(r'$k,\,\mathregular{km^{-1}}$')
+ax.grid(which='major', axis='both', color='gray', alpha=0.5)
+
+ax.legend()
+
+# Uncomment for setting X-Y limits on the plot
+# ax.set_xlim()
+# ax.set_ylim(bottom=1.1, top=1.4)
+
+# Uncomment for setting a title
+# plt.title(r'(species) Absorption Coefficient in $\mathregular{km^{-1}}$ at (pressure value) in the (range)' 
+#             r'$\mathregular{cm^{-1}}$ range for (line shape)')
+
+plt.tight_layout()
+plt.show()
