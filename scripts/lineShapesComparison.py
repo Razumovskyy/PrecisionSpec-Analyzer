8bba109919e495f0e56af64167a7b0b1d2ae522c
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2024-04-03 14:32:48 +0400

    fixes for lineShape plot script

--- scripts/lineShapesComparison.py
+++ scripts/lineShapesComparison.py
@@ -6,16 +6,16 @@ refWavenumber, lineShape2 = np.loadtxt(fname='./lineShape1Data.dat', unpack=True
 
 # wavenumber, lineShape1, lineShape2 = np.loadtxt(fname='./lineShapesData.dat', unpack=True, delimiter=',')
 
-ax, fig = plt.subplots()
+fig, ax = plt.subplots()
 
-small_value = 1e-45
+small_value = 1e-10
 lineShape2[lineShape2 < small_value] = small_value
 
-ax.plot(wavenumber, np.log(lineShape1), color='r', label='LOR')
-ax.plot(refWavenumber, np.log(lineShape2), color='b', label='DOP')
+ax.plot(wavenumber, np.log10(lineShape1), color='r', label='LOR')
+ax.plot(refWavenumber, np.log10(lineShape2), color='b', label='DOP')
 
 ax.set_xlabel(r'wavenumber, $\mathregular{cm^{-1}}$')
-ax.set_ylabel(r'$k,\,\mathregular{cm}$')
+ax.set_ylabel(r'$l,\,\mathregular{cm}$')
 ax.grid(which='major', axis='both', color='gray', alpha=0.5)
 
 # Uncomment for setting a title
