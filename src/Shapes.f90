a8cac492364717db8c2e418e4a47354b167ce02e
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2024-04-06 20:28:26 +0400

    add variants for lineShapes functions

--- src/Shapes.f90
+++ src/Shapes.f90
@@ -6,21 +6,61 @@ module Shapes
 contains
     
     real function simpleLorentz(X)
+        !! 
+        ! Simplistic Lorentz line shape function in which there is no account for self-broadening and no temperature dependence
+        !!
         ! X - [cm-1] -- distance from the shifted line center to the spectral point in which the total contribution from lines is calculated
         real(kind=DP), intent(in) :: X
         ! -------------------------------------------------------- !
-        
         real(kind=DP) :: HWHM
 
         HWHM = lorentzHWHM(pressure, includeGammaSelf=.false., includeTemperature=.false.)
         simpleLorentz = HWHM / (pi*(X**2 + HWHM**2))
     end function simpleLorentz
 
+    real function selfSimpleLorentz(X)
+        !! 
+        ! Lorentz line shape function in which there is no temperature dependence (T = 296 K), but self-broadening is taken into account
+        !!
+        ! X - [cm-1] -- distance from the shifted line center to the spectral point in which the total contribution from lines is calculated
+        real(kind=DP), intent(in) :: X
+        ! -------------------------------------------------------- !
+        real(kind=DP) :: HWHM
+
+        HWHM = lorentzHWHM(pressure, includeGammaSelf=.true., includeTemperature=.false.)
+        selfSimpleLorentz = HWHM / (pi*(X**2 + HWHM**2))
+    end function selfSimpleLorentz
+
+    real function noSelfLorentz(X)
+        !! 
+        ! Temperature-dependent Lorentz line shape with no account for self-broadening
+        !!
+        ! X - [cm-1] -- distance from the shifted line center to the spectral point in which the total contribution from lines is calculated
+        real(kind=DP), intent(in) :: X
+        ! -------------------------------------------------------- !
+        real(kind=DP) :: HWHM
+
+        HWHM = lorentzHWHM(pressure, includeGammaSelf=.true., includeTemperature=.true., temperatureParameter=temperature)
+        noSelfLorentz = HWHM / (pi*(X**2 + HWHM**2))
+    end function noSelfLorentz
+
+    real function lorentz(X)
+        !! 
+        ! Temperature-dependent Lorentz line shape with no account for self-broadening
+        !!
+        ! X - [cm-1] -- distance from the shifted line center to the spectral point in which the total contribution from lines is calculated
+        real(kind=DP), intent(in) :: X
+        ! -------------------------------------------------------- !
+        real(kind=DP) :: HWHM
+
+        HWHM = lorentzHWHM(pressure, includeGammaSelf=.true., includeTemperature=.true., temperatureParameter=temperature)
+        lorentz = HWHM / (pi*(X**2 + HWHM**2))
+    end function lorentz
+
     real function doppler(X)
         ! X - [cm-1] -- distance from the shifted line center to the spectral point in which the total contribution from lines is calculated
         real(kind=DP), intent(in) :: X
         ! -------------------------------------------------------- !
-
         real(kind=DP) :: HWHM ! [cm-1] -- Doppler HWHM
   
         HWHM = dopplerHWHM(lineWV, temperature, molarMass)        
