91670cfb7b0211bd8330e796cddf6616d85115da
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2024-04-07 17:03:12 +0400

    add self-broadening in atmospheric data

--- src/Atmosphere.f90
+++ src/Atmosphere.f90
@@ -2,6 +2,7 @@ module Atmosphere
     use Constants
     implicit none
     real :: pressure ! [atm] -- pressure on the current atmospheric level
+    real :: pSelf ! [atm] -- partial pressure of the considered gaseous species
     real :: temperature ! [K] -- temperauture on the current atmospheric level
     real :: density ! [molecule/(cm^2 * km)] -- such density units are needed for having absorption coefficient in km-1
 contains
@@ -10,5 +11,6 @@ contains
         pressure = 1. ! ~ Venus 50 km level (see data/Atmospheres/H2O_117.dat)
         density = 0.6994E+20 ! ~ Venus 50 km level (see data/Atmospheres/H2O_117.dat)
         temperature = refTemperature
+        pSelf = 0.01
     end subroutine fetchAtmosphericParameters
 end module Atmosphere
