e7f3e766bc858c0b81bf4d2962fc6b48eac60d63
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2024-05-08 10:06:37 +0400

    ref: add TODO

--- src/Atmosphere.f90
+++ src/Atmosphere.f90
@@ -8,12 +8,17 @@ module Atmosphere
     real :: density ! [molecule/(cm^2 * km)] -- such density units are needed for having absorption coefficient in km-1
 contains
     subroutine fetchAtmosphericParameters()
-        ! read from atmospheric file later
-        pressure = 1. ! ~ Venus 50 km level (see data/Atmospheres/H2O_117.dat)
-        density = 0.6994E+20 ! ~ Venus 50 km level (see data/Atmospheres/H2O_117.dat)
+        ! TODO: read from atmospheric file later
+        ! pressure = 1 ! ~ Venus 50 km level (see data/Atmospheres/H2O_117.dat)
+        ! density = 0.6994E+20 ! ~ Venus 50 km level (see data/Atmospheres/H2O_117.dat)
+        
+        pressure = 0.44126E-02 ! ~ Venus 80 km level
+        density = 0.4782E+17 ! ~ Venus 80 km level (see data/Atmospheres/H2O_117.dat)
         ! temperature = refTemperature
-        temperature = 400.
-        pSelf = 0.01
+        temperature = 203.
+        
+        pSelf = density * 10. / LOSCHMIDT * temperature/stTemperature
+        ! pSelf = 3.8113994E-5
         pForeign = pressure - pSelf
     end subroutine fetchAtmosphericParameters
 end module Atmosphere
